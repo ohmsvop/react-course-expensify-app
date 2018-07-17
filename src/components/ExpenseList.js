774173d185b563a960361ad94607c74ca785e373
Yi-An Wu <ohmsvop@gmail.com>
2018-07-17 17:16:13 +0800

    Add expense summary to dashboard

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -6,7 +6,6 @@ import selectExpenses from '../selectors/expenses';
 export const ExpenseList = (props) => (
     <div>
         <h1>Expense List</h1>
-
         {
             props.expenses.length === 0 ? (
                 <p>No expenses</p>
