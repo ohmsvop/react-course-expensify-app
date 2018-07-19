e84ea82f95c85864a5c3593578a8b557050b6dd3
Yi-An Wu <ohmsvop@gmail.com>
2018-07-19 18:25:03 +0800

    Setup test database env

---
+++ src/firebase/firebase.js
@@ -0,0 +1,51 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+};
+
+firebase.initializeApp(config);
+
+const database = firebase.database();
+
+export { firebase, database as default };
+
+// database.ref().on('value', (snapshot) => {
+//     value = snapshot.val()
+//     console.log(`${value.name}`);
+// });
+
+// database.ref()
+//     .once('value')
+//     .then((snapshot) => {
+//         const val = snapshot.val();
+//         console.log(val);
+//     }).catch((e) => {
+//         console.log('Error fetching data', e);
+//     });
+// database.ref().set({
+//     name: 'Mars',
+//     age: 29,
+//     location: {
+//         city: 'Taipei',
+//         country: 'Taiwan'
+//     }
+// }).then(() => {
+//     console.log('Data is saved');
+// }).catch((e) => {
+//     console.log('This.failed.', e);
+// });
+
+// database.ref('age').set(26);
+// database.ref('location/city').set('Tokyo');
+// database.ref('attributes').set({
+//     height: 160,
+//     weight: 60
+// });
+
+// database.ref('location/city').remove();
